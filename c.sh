4a229d334ab2671d4bbfbe7ccd588e075892889d
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2025-03-01 11:43:28 +0100

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+
+echo "deb http://deb.debian.org/debian sid main contrib non-free non-free-firmware" > /etc/apt/sources.list
 apt update && apt upgrade -y
 apt install locales -y
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+echo "debian" > /etc/hostname
+echo "127.0.0.1 localhost" > /etc/hosts
+echo "127.0.1.1 debian" >> /etc/hosts
+
+apt install linux-image-amd64 firmware-linux firmware-linux-nonfree
+
+echo "KEYMAP=fr" = /etc/vconsole.conf
+
+ln -sf /usr/share/zoneinfo/Africa/Algiers /etc/localtime
+hwclock --systohc
+
